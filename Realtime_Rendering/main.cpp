70857ec333e4e87fececbe39186af3f144990eda
ByeongGyoo Kim <BangGyoo12@gmail.com>
2018-10-04 15:54:19 +0900

    Update perspective projection function

--- Realtime_Rendering/main.cpp
+++ Realtime_Rendering/main.cpp
@@ -22,12 +22,32 @@ glm::mat4 LookAt(glm::vec3 campos, glm::vec3 look, glm::vec3 up) {
 	return result;
 }
 
+glm::mat4 perspective(float fovy, float aspect, float near, float far) {
+
+	glm::mat4 result(
+		1.0 / (aspect*tan(glm::radians(fovy / 2))), 0, 0, 0,
+		0, 1.0 / tan(glm::radians(fovy / 2)), 0, 0,
+		0, 0, -((far + near) / (far - near)), -1,
+		0, 0, -((2 * far * near) / (far - near)), 0);
+	return result;
+}
+
 int main() {
 
-	glm::vec3 cameraPosition(5), cameraTarget(0);
-	glm::vec3 upVector(0,1,0);
+	//glm::vec3 cameraPosition(5), cameraTarget(0);
+	//glm::vec3 upVector(0,1,0);
 
-	glm::mat4 result = LookAt(cameraPosition, cameraTarget, upVector);
+	//glm::mat4 result = LookAt(cameraPosition, cameraTarget, upVector);
+
+	float W = 780;
+	float H = 750;
+	float FovY = 45.0f;
+	float NearZ = 0.1f;
+	float FarZ = 500.0f;
+
+	float Aspect = W / H;
+	glm::mat4 result = perspective(FovY, Aspect, NearZ, FarZ);
+
 
 	std::cout << glm::to_string(result) << std::endl;
 
